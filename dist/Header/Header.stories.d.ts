dab16e08963b969f68632c5523cd744dadc9ad7c
David Nicholas <Dnicholas@bitovi.com>
2022-12-12 16:30:35 -0800

    add dist

---
+++ dist/Header/Header.stories.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="react" />
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+declare const _default: ComponentMeta<({ title }: import("./Header").HeaderProps) => JSX.Element>;
+export default _default;
+export declare const Without_Title: ComponentStory<({ title }: import("./Header").HeaderProps) => JSX.Element>;
+export declare const With_Title: ComponentStory<({ title }: import("./Header").HeaderProps) => JSX.Element>;
